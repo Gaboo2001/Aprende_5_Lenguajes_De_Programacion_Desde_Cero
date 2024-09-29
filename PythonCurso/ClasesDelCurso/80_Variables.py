94f804cebcdbb69b50f413c92797e446ec7a2538
Gaboh <22030263@itesa.edu.mx>
2024-09-29 13:42:39 -0600

    29/09/2024

---
+++ PythonCurso/ClasesDelCurso/80_Variables.py
@@ -0,0 +1,14 @@
+numero = 2
+print(numero)
+numero = 3
+print(numero) #Aun que estamos usando la primera variable, en ella primero guardamos un valor y despues le asiganamos otro valor
+
+print("")
+
+a = 2
+b = 3
+c = a+b
+print(c)
+
+
+
---
+++ PythonCurso/ClasesDelCurso/81_TiposDeDatos.py
@@ -0,0 +1,14 @@
+entero = 7
+print(type(entero))
+
+decimal = 12.3
+print(type(decimal))
+
+complejo = 1 + 3j
+print(type(complejo))
+
+texto = "TIPO DE DATOS"
+print(type(texto))
+
+booleano = False
+print(type(booleano))
