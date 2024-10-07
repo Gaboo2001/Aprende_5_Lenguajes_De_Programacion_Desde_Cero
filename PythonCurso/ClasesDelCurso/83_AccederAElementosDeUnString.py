c89fd0d309eff7aea9f08d9aa048f000f35e754e
Gaboh <22030263@itesa.edu.mx>
2024-10-07 00:40:26 -0600

    07/10/2024

---
+++ PythonCurso/ClasesDelCurso/83_AccederAElementosDeUnString.py
@@ -0,0 +1,26 @@
+texto = "Estamos programando en python"
+print(texto)
+print(texto[0])
+print(texto[3])
+print(texto[8])
+print(texto[-1])
+#Lo que se hizo anteriormente fue impirmir la posicion de algunas letras y no  todo el texto
+
+print(texto [0:7]) #tambien podemos imprimir solo una parte si la necesitamos como en este caso solo se va a impimir la
+#palabra estamos
+print(texto [:19]) #esta es otra forma
+
+print(texto [8:])
+
+print(texto [8:19])
+#VARIAS FORAMAS DE COMO PODEMOS OBTENER DATOS DE UN STRING
+
+#Ahora para ver como podemos obtener datos pero de 2 en 2 o de mas elementos
+print(texto [0:29:2]) #Aqui podemos ver como obtine todo lo que esta en el string pero de 2 en dos lugares o posiciones
+
+print(texto [8::2])
+
+print(texto [::-1]) #FORMA PARA IMPRIMIR TODA LA CADENA PERO AL REVEZ
+
+
+
